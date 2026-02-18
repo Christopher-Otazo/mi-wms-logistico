601613d1d422c88851f29cb2224df5be03d4c2f1
Christopher-Otazo <chrisotazo77@gmail.com>
2026-02-18 13:54:06 -0300

    feat: upgrade de sistema con menu interactivo

--- inventario.py
+++ inventario.py
@@ -1,9 +1,8 @@
 import sqlite3
 
 def inicializar_db():
-
-    conexion = sqlite3.connect('bodega.db')
-    cursor = conexion.cursor()
+    conn = sqlite3.connect('bodega.db')
+    cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS productos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,41 +11,84 @@ def inicializar_db():
             cantidad INTEGER DEFAULT 0,
             punto_critico INTEGER DEFAULT 5,
             ubicacion TEXT
-            )
-     ''')
-    conexion.commit()
-    conexion.close()
-def agregar_producto(sku, nombre, cantidad, punto_critico, ubicacion):
-    try:
-        conn = sqlite3.connect('bodega.db')
-        cursor = conn.cursor()
-        cursor.execute('''
-             INSERT INTO productos (sku, nombre, cantidad, punto_critico, ubicacion)
-             VALUES (?, ?, ?, ?, ?)
-             ''', (sku, nombre, cantidad, punto_critico, ubicacion))
-        conn.commit()
-        print(f"Producto {nombre} registrado en rack {ubicacion}.")
-    except sqlite3.IntegrityError:
-                    print(f"El SKU {sku} ya existe, saltando registro.")
-    finally:
-         conn.close()
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-def reporte_stock_critico():
+def realizar_movimiento(sku, cantidad_cambio):
+    """
+    Suma o resta stock.
+    cantidad_cambio positiva = Entrada (Recepcion)
+    cantidad_cambio negativa = Salida (Despacho/Pickeo)
+    """
+
     conn = sqlite3.connect('bodega.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT sku, nombre, cantidad, punto_critico FROM productos WHERE cantidad <= punto_critico')
-    alertas = cursor.fetchall()
 
-    print("\n--- ALERTAS DE REPOSICION (Logistica SAP)---")
-    if not alertas:
-        print("Todo en orden. Stock suficiente.")
-    for p in alertas:
-        print(f"SKU: {p[0]} | {p[1]} - Stock: {p[2]} (Minimo: {p[3]})")
+    #1. Verificar si existe y cuanto hay
+    cursor.execute('SELECT cantidad, nombre FROM productos WHERE sku = ?', (sku,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        stock_actual = resultado[0]
+        nombre = resultado[1]
+        nuevo_stock = stock_actual + cantidad_cambio
+
+        if nuevo_stock < 0:
+            print(f"ERROR: Stock insuficiente de {nombre}. Disponible: {stock_actual}")
+        else:
+            cursor.execute('UPDATE productos SET cantidad = ? WHERE sku = ?', (nuevo_stock, sku))
+            conn.commit()
+            tipo = "Entrada" if cantidad_cambio > 0 else "Salida"
+            print(f"{tipo} registrada. {nombre} ahora tiene {nuevo_stock} unidades.")
+    else:
+        print("Error: SKU no encontrado.")
+
     conn.close()
-    
-if __name__ == "__main__":
-     inicializar_db()
-     print("Simulando ingreso de mercaderia...")
-     agregar_producto("P&G-001", "Detergente Ace 1L", 20, 10, "A-12")
-     agregar_producto("P&G-002", "Shampoo Head & Shoulders", 3, 15, "B-05")
-     reporte_stock_critico()
+
+def mostrar_menu():
+    print("\n--- LOGISTOCK-CLI v2.0 ---")
+    print("1. Ver Inventario Completo")
+    print("2. Registrar Entrada")
+    print("3. Registrar Salida")
+    print("4. Ver Alertas de Reposicion")
+    print("5. Salir")
+    return input("Seleccione una opcion:  ")
+
+def listar_productos():
+    conn = sqlite3.connect('bodega.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT sku, nombre, cantidad, ubicacion FROM productos')
+    for p in cursor.fetchall():
+        print(f"[{p[3]}] SKU: {p[0]} | {p[1]} | Stock: {p[2]}")
+    conn.close()
+
+if __name__== "__main__":
+    inicializar_db()
+    while True:
+        opcion = mostrar_menu()
+        if opcion == "1":
+            listar_productos()
+        elif opcion == "2":
+            s = input("SKU: ")
+            c = int(input("Cantidad a entrar: "))
+            realizar_movimiento(s, c)
+        elif opcion == "3":
+            s = input("SKU: ")
+            c = int(input("Cantidad a despachar: "))
+            realizar_movimiento(s, -c) #Se envia negativo para restar
+        elif opcion == "4":
+            def reporte_stock_critico():
+                conn = sqlite3.connect('bodega.db')
+                cursor = conn.cursor()
+                cursor.execute('SELECT sku, nombre, cantidad, punto_critico FROM productos WHERE cantidad <= punto_critico')
+                alertas = cursor.fetchall()
+                print("\n--- ALERTA DE REPOSICION (Logica SAP) ---")
+                if not alertas:
+                    print("Todo en orden. Stock suficiente.")
+                for p in alertas:
+                    print(f"SKU: {p[0]} | {p[1]} - Stock: {p[2]} (Minimo: {p[3]})")
+        elif opcion == "5":
+            print("Cerrando Sistema...")
+            break
